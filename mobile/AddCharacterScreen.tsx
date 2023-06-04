0207bd359bca8d3ea57bc3b9f2e9fefba3a541b0
Jun-10-r <junior.codemaster@gmail.com>
2023-06-04 19:11:49 -0300

    construcao do Front

---
+++ mobile/AddCharacterScreen.tsx
@@ -0,0 +1,11 @@
+import React, { useState} from 'react';
+import { View, TextInput, Button, Alert  } from 'react-native';
+import axios from 'axios';
+
+
+
+const AddCharacterScreen = () => {
+    const [habitat, setHabitat] = useState('');
+    const [comidaFavorita, setComidaFavorita] = useState('');
+    //outros campos de caracteristicas aqui...
+}
