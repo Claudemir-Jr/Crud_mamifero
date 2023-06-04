afb9c21bb95860e377a40c6cdd0e304bf2003779
Jun-10-r <junior.codemaster@gmail.com>
2023-06-04 19:48:00 -0300

    adicao na tela ListCharactersScreen.tsx dos botoes

--- mobile/AddCharacterScreen.tsx
+++ mobile/AddCharacterScreen.tsx
@@ -4,6 +4,7 @@
 import React, { useState} from 'react';
 import { View, TextInput, Button, Alert  } from 'react-native';
 import axios from 'axios';
+import { NavigationScreenProp } from 'react-navigation';
 
 
 
@@ -42,6 +43,50 @@ const adicionarCaracteristicas = async () => {
       <Button title="Adicionar" onPress={adicionarCaracteristicas} />
     </View>
   );
+  
+};
+
+interface Props {
+  navigation: NavigationScreenProp<any, any>;
+}
+
+const AddCharacterScreen = ({ navigation }: Props) => {
+  const [habitat, setHabitat] = useState('');
+  const [comidaFavorita, setComidaFavorita] = useState('');
+
+  const adicionarCaracteristicas = async () => {
+    try {
+      await axios.post('http://seu-backend.com/adicionar', {
+        habitat,
+        comidaFavorita,
+      });
+      Alert.alert('Sucesso', 'Características salvas!');
+    } catch (error) {
+      Alert.alert('Erro', 'Não foi possível adicionar as características.');
+    }
+  };
+
+  return (
+    <View>
+      <TextInput
+        placeholder="Habitat"
+        value={habitat}
+        onChangeText={setHabitat}
+      />
+      <TextInput
+        placeholder="Comida Favorita"
+        value={comidaFavorita}
+        onChangeText={setComidaFavorita}
+      />
+      <Button title="Adicionar" onPress={adicionarCaracteristicas} />
+
+      <Button
+        title="Ver Características"
+        onPress={() => navigation.navigate('ListCharacters')}
+      />
+    </View>
+  );
 };
 
 export default AddCharacterScreen;
+
--- mobile/ListCharactersScreen.tsx
+++ mobile/ListCharactersScreen.tsx
@@ -4,6 +4,7 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
 import axios from 'axios';
+import { NavigationScreenProp } from 'react-navigation';
 
 interface Characteristic {
   id: number;
@@ -41,5 +42,43 @@ const ListCharactersScreen = () => {
     </View>
   );
 };
+
+interface Props {
+    navigation: NavigationScreenProp<any, any>;
+  }
+  
+  const ListCharactersScreen = ({ navigation }: Props) => {
+    const [caracteristicas, setCaracteristicas] = useState<Characteristic[]>([]);
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await axios.get<Characteristic[]>(
+            'http://seu-backend.com/listar'
+          );
+          setCaracteristicas(response.data);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+  
+      fetchData();
+    }, []);
+  
+    return (
+      <View>
+        {caracteristicas.map(caracteristica => (
+          <Text key={caracteristica.id}>
+            {caracteristica.habitat} - {caracteristica.comidaFavorita}
+          </Text>
+        ))}
+  
+        <Button
+          title="Voltar"
+          onPress={() => navigation.navigate('AddCharacter')}
+        />
+      </View>
+    );
+  };
 
 export default ListCharactersScreen;
