1479b8fb347f6c05265d934420d6df049b7d04d5
Jun-10-r <junior.codemaster@gmail.com>
2023-06-05 00:31:27 -0300

    ajuste eatualizacao sobre tipagem dos campos das telas editarscreen e listarscreen

--- front_onca/src/screens/EditarScreen.tsx
+++ front_onca/src/screens/EditarScreen.tsx
@@ -1,9 +1,19 @@
 import React, { useState } from 'react';
 import { View, TextInput, Button, Alert } from 'react-native';
 import axios from 'axios';
-import { RouteProp, NavigationProp } from '@react-navigation/native';
+import { RouteProp } from '@react-navigation/native';
 
-const EditarScreen = ({ route }) => {
+type RootStackParamList = {
+  Editar: { id: string };
+};
+
+type EditarScreenRouteProp = RouteProp<RootStackParamList, 'Editar'>;
+
+type Props = {
+  route: EditarScreenRouteProp;
+};
+
+const EditarScreen: React.FC<Props> = ({ route }) => {
   const { id } = route.params;
   const [habitat, setHabitat] = useState('');
   const [comidaFavorita, setComidaFavorita] = useState('');
@@ -70,3 +80,159 @@ const EditarScreen = ({ route }) => {
 };
 
 export default EditarScreen;
+
+
+
+
+// import React, { useState } from 'react';
+// import { View, TextInput, Button, Alert } from 'react-native';
+// import axios from 'axios';
+// import { RouteProp } from '@react-navigation/native';
+
+// type Props = {
+//   route: RouteProp<any, any>;
+// };
+
+// const EditarScreen: React.FC<Props> = ({ route }) => {
+//   const { id } = route.params;
+//   const [habitat, setHabitat] = useState('');
+//   const [comidaFavorita, setComidaFavorita] = useState('');
+//   const [descricao, setDescricao] = useState('');
+//   const [quantidadePatas, setQuantidadePatas] = useState('');
+//   const [sexo, setSexo] = useState('');
+//   const [hibernacao, setHibernacao] = useState('');
+
+//   const editarCaracteristicas = async () => {
+//     try {
+//       const response = await axios.put(`http://localhost:3000/editar/${id}`, {
+//         habitat,
+//         comidaFavorita,
+//         descricao,
+//         quantidadePatas: parseInt(quantidadePatas),
+//         sexo,
+//         hibernacao: hibernacao === 'true',
+//       });
+
+//       if (response.status === 205) {
+//         Alert.alert('Sucesso', 'Características alteradas com sucesso!');
+//       }
+//     } catch (error) {
+//       Alert.alert('Erro', 'Ocorreu um erro ao editar as características.');
+//     }
+//   };
+
+//   return (
+//     <View>
+//       <TextInput
+//         placeholder="Habitat"
+//         value={habitat}
+//         onChangeText={setHabitat}
+//       />
+//       <TextInput
+//         placeholder="Comida Favorita"
+//         value={comidaFavorita}
+//         onChangeText={setComidaFavorita}
+//       />
+//       <TextInput
+//         placeholder="Descrição"
+//         value={descricao}
+//         onChangeText={setDescricao}
+//       />
+//       <TextInput
+//         placeholder="Quantidade de Patas"
+//         value={quantidadePatas}
+//         onChangeText={setQuantidadePatas}
+//         keyboardType="numeric"
+//       />
+//       <TextInput
+//         placeholder="Sexo"
+//         value={sexo}
+//         onChangeText={setSexo}
+//       />
+//       <TextInput
+//         placeholder="Hibernação"
+//         value={hibernacao}
+//         onChangeText={setHibernacao}
+//       />
+//       <Button title="Editar" onPress={editarCaracteristicas} />
+//     </View>
+//   );
+// };
+
+// export default EditarScreen;
+
+
+
+
+// import React, { useState } from 'react';
+// import { View, TextInput, Button, Alert } from 'react-native';
+// import axios from 'axios';
+// import { RouteProp, NavigationProp } from '@react-navigation/native';
+
+// const EditarScreen = ({ route }) => {
+//   const { id } = route.params;
+//   const [habitat, setHabitat] = useState('');
+//   const [comidaFavorita, setComidaFavorita] = useState('');
+//   const [descricao, setDescricao] = useState('');
+//   const [quantidadePatas, setQuantidadePatas] = useState('');
+//   const [sexo, setSexo] = useState('');
+//   const [hibernacao, setHibernacao] = useState('');
+
+//   const editarCaracteristicas = async () => {
+//     try {
+//       const response = await axios.put(`http://localhost:3000/editar/${id}`, {
+//         habitat,
+//         comidaFavorita,
+//         descricao,
+//         quantidadePatas: parseInt(quantidadePatas),
+//         sexo,
+//         hibernacao: hibernacao === 'true',
+//       });
+
+//       if (response.status === 205) {
+//         Alert.alert('Sucesso', 'Características alteradas com sucesso!');
+//       }
+//     } catch (error) {
+//       Alert.alert('Erro', 'Ocorreu um erro ao editar as características.');
+//     }
+//   };
+
+//   return (
+//     <View>
+//       <TextInput
+//         placeholder="Habitat"
+//         value={habitat}
+//         onChangeText={setHabitat}
+//       />
+//       <TextInput
+//         placeholder="Comida Favorita"
+//         value={comidaFavorita}
+//         onChangeText={setComidaFavorita}
+//       />
+//       <TextInput
+//         placeholder="Descrição"
+//         value={descricao}
+//         onChangeText={setDescricao}
+//       />
+//       <TextInput
+//         placeholder="Quantidade de Patas"
+//         value={quantidadePatas}
+//         onChangeText={setQuantidadePatas}
+//         keyboardType="numeric"
+//       />
+//       <TextInput
+//         placeholder="Sexo"
+//         value={sexo}
+//         onChangeText={setSexo}
+//       />
+//       <TextInput
+//         placeholder="Hibernação"
+//         value={hibernacao}
+//         onChangeText={setHibernacao}
+//       />
+//       <Button title="Editar" onPress={editarCaracteristicas} />
+//     </View>
+//   );
+// };
+
+// export default EditarScreen;
--- front_onca/src/screens/ListarScreen.tsx
+++ front_onca/src/screens/ListarScreen.tsx
@@ -2,13 +2,23 @@ import React, { useEffect, useState } from 'react';
 import { View, Text, FlatList } from 'react-native';
 import axios from 'axios';
 
+type Caracteristica = {
+  habitat: string;
+  comidaFavorita: string;
+  descricao: string;
+  quantidadePatas: number;
+  sexo: string;
+  hibernacao: boolean;
+  id: number;
+};
+
 const ListarScreen = () => {
-  const [caracteristicas, setCaracteristicas] = useState([]);
+  const [caracteristicas, setCaracteristicas] = useState<Caracteristica[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://localhost:3000/listar');
+        const response = await axios.get<Caracteristica[]>('http://localhost:3000/listar');
         setCaracteristicas(response.data);
       } catch (error) {
         console.log(error);
@@ -39,3 +49,46 @@ const ListarScreen = () => {
 };
 
 export default ListarScreen;
+
+
+// import React, { useEffect, useState } from 'react';
+// import { View, Text, FlatList } from 'react-native';
+// import axios from 'axios';
+
+// const ListarScreen = () => {
+//   const [caracteristicas, setCaracteristicas] = useState([]);
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const response = await axios.get('http://localhost:3000/listar');
+//         setCaracteristicas(response.data);
+//       } catch (error) {
+//         console.log(error);
+//       }
+//     };
+
+//     fetchData();
+//   }, []);
+
+//   return (
+//     <View>
+//       <FlatList
+//         data={caracteristicas}
+//         renderItem={({ item }) => (
+//           <View>
+//             <Text>Habitat: {item.habitat}</Text>
+//             <Text>Comida Favorita: {item.comidaFavorita}</Text>
+//             <Text>Descrição: {item.descricao}</Text>
+//             <Text>Quantidade de Patas: {item.quantidadePatas}</Text>
+//             <Text>Sexo: {item.sexo}</Text>
+//             <Text>Hibernação: {item.hibernacao ? 'Sim' : 'Não'}</Text>
+//           </View>
+//         )}
+//         keyExtractor={(item) => item.id.toString()}
+//       />
+//     </View>
+//   );
+// };
+
+// export default ListarScreen;
